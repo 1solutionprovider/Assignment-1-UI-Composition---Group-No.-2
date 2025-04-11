f10c8a84ac8d4082467c1448d766b204dbfc2086
Sameer Ahmed Sahito <sameeerqadir2004@gmail.com>
2025-04-12 02:53:23 +0500

    integerated Tweetcard widget into Listview with sample tweetcards

--- part3/lib/main.dart
+++ part3/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'tweet_card.dart'; // Import the custom widget
 
 void main() => runApp(MyTwitterClone());
 
@@ -16,28 +17,41 @@ class MyTwitterClone extends StatelessWidget {
 class TwitterHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-   
-return Scaffold(
-  appBar: AppBar(
-    title: Text("Home", style: TextStyle(color: Colors.black)),
-    backgroundColor: Colors.white,
-  ),
-  body: ListView(
-  children: [
-   
-  ],
-),
-  bottomNavigationBar: BottomNavigationBar(
-    currentIndex: 0,
-    selectedItemColor: Colors.blue,
-    unselectedItemColor: Colors.grey,
-    items: [
-      BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-      BottomNavigationBarItem(icon: Icon(Icons.search), label: "Explore"),
-      BottomNavigationBarItem(icon: Icon(Icons.notifications_none), label: "Notifications"),
-      BottomNavigationBarItem(icon: Icon(Icons.mail_outline), label: "Messages"),
-    ],
-  ),
-);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home", style: TextStyle(color: Colors.black)),
+        backgroundColor: Colors.white,
+      ),
+      body: ListView(
+        children: [
+          TweetCard(
+            userName: "Sameer Ahmed",
+            userHandle: "@Sameer_1",
+            time: "2m",
+            tweetText: "This is my sample tweet with an image!",
+            tweetImage: "https://via.placeholder.com/350x150",
+          ),
+          TweetCard(
+            userName: "Ameer umar khan",
+            userHandle: "@Ameerumar_1",
+            time: "15m",
+            tweetText: "Loving the UI design. Flutter is fun!",
+            tweetImage: null,
+          ),
+          
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 0,
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: "Explore"),
+          BottomNavigationBarItem(icon: Icon(Icons.notifications_none), label: "Notifications"),
+          BottomNavigationBarItem(icon: Icon(Icons.mail_outline), label: "Messages"),
+        ],
+      ),
+    );
   }
 }
