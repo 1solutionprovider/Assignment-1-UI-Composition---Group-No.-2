1163f95cce878035f3bee04fadeeae5739a040d4
Sameer Ahmed Sahito <sameeerqadir2004@gmail.com>
2025-04-13 23:37:06 +0500

    Added MyApp widget skeleton with AppBar and Scacffold

--- main.dart
+++ main.dart
@@ -1,3 +1,24 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const String appTitle = 'Flutter layout demo';
+    return MaterialApp(
+      title: appTitle,
+      home: Scaffold(
+        appBar: AppBar(title: const Text(appTitle)),
+        body: const SingleChildScrollView(
+          child: Column(
+            children: [
+              // Widgets will be inserted here in later commits.
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
