9ffaab4f30d18aa2741d71270fd2014db7d6a3d0
Sameer Ahmed Sahito <sameeerqadir2004@gmail.com>
2025-04-13 23:43:10 +0500

    final cleanup and code formatting adjustment for layout demo

--- main.dart
+++ main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
                     'Bernese Alps. Situated 1,578 meters above sea level, it '
                     'is one of the larger Alpine Lakes. A gondola ride from '
                     'Kandersteg, followed by a half-hour walk through pastures '
-                    'and pine forest, leads you to the lake, which warms to 20 '
+                    'and pineee forest, leads you to the lake, which warms to 20 '
                     'degrees Celsius in the summer. Activities enjoyed here '
                     'include rowing, and riding the summer toboggan run.',
               ),
