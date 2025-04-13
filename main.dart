3cd30333faebc3ff82eab982f36c0c9f8dd33d0e
Sameer Ahmed Sahito <sameeerqadir2004@gmail.com>
2025-04-13 23:40:30 +0500

    Added TitleSection widget and update layout columnn

--- main.dart
+++ main.dart
@@ -13,12 +13,17 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(title: const Text(appTitle)),
         body: const SingleChildScrollView(
           child: Column(
-            children: [
-              children: [
-  ImageSection(
-    image: 'images/lake.jpg.jpg',
+             children: [
+  ImageSection(image: 'images/lake.jpg.jpg'),
+  TitleSection(
+    name: 'Oeschinen Lake Campground',
+    location: 'Kandersteg, Switzerland',
   ),
-  // Other widgets will follow.
+ 
+],
+
+  ),
+
 ],
 
             ],
@@ -40,6 +45,43 @@ class ImageSection extends StatelessWidget {
       width: 600,
       height: 240,
       fit: BoxFit.cover,
+    );
+  }
+}
+class TitleSection extends StatelessWidget {
+  const TitleSection({super.key, required this.name, required this.location});
+
+  final String name;
+  final String location;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: Text(
+                    name,
+                    style: const TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Text(
+                  location,
+                  style: TextStyle(color: Colors.grey[500]),
+                ),
+              ],
+            ),
+          ),
+          Icon(Icons.star, color: Colors.red[500]),
+          const Text('41'),
+        ],
+      ),
     );
   }
 }
